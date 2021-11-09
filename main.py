48969f0bc3c6026c622dd7375a7f7686627a73d7
sebast14 <93997652+sebast14@users.noreply.github.com>
2021-11-09 10:49:34 -0500

    Imprimir en python

---
+++ main.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+message = "Espacio en memoria"
+number = 12
+float_var = 12.12
+
+print(message)
+print(type (message))
+
+print(number)
+print(type (number))
+
+print(float_var)
+print(type (float_var))
