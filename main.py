e487de58914b46dcabbb59e75b3c797da6ede9b9
sebast14 <93997652+sebast14@users.noreply.github.com>
2021-11-09 11:12:18 -0500

    Tarea #1

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 message = "Espacio en memoria"
 number = 12
 float_var = 12.12
+nnumber1 = 12
+nnumber2 = 3
+
+nnumber1f = 12.1
+nnumber2f = 3.1
 
 print(message)
 print(type (message))
@@ -13,3 +18,21 @@ print(type (number))
 
 print(float_var)
 print(type (float_var))
+
+message1 = "Suma de dos int " 
+print(message1)
+
+print ("Primer numero =",nnumber1) 
+print ("Segundo numero =",nnumber2)
+print ("Resultado = ",nnumber1+nnumber2)
+
+message2 = "Suma de dos float " 
+print(message2)
+
+print ("Primer numero =",nnumber1f) 
+print ("Segundo numero =",nnumber2f)
+print ("Resultado = ",nnumber1f+nnumber2f)
+
+message3 = "hola"
+message4 = " mundo"
+print(message3 + message4)
