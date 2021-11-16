02d3190a60972ba216689442c1160a83b8ff4dd6
sebast14 <93997652+sebast14@users.noreply.github.com>
2021-11-16 12:02:11 -0500

    Area de un circulo

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
 
-message = "Espacio en memoria"
-number = 12
-float_var = 12.12
-nnumber1 = 12
-nnumber2 = 3
+from math import pi
 
-nnumber1f = 12.1
-nnumber2f = 3.1
+r = 2
+area = pi * r **2
+print("El radio de la circunferencia es ",r)
+print("Y su area es", area)
 
-print(message)
-print(type (message))
 
-print(number)
-print(type (number))
-
-print(float_var)
-print(type (float_var))
-
-message1 = "Suma de dos int " 
-print(message1)
-
-print ("Primer numero =",nnumber1) 
-print ("Segundo numero =",nnumber2)
-print ("Resultado = ",nnumber1+nnumber2)
-
-message2 = "Suma de dos float " 
-print(message2)
-
-print ("Primer numero =",nnumber1f) 
-print ("Segundo numero =",nnumber2f)
-print ("Resultado = ",nnumber1f+nnumber2f)
-
-message3 = "hola"
-message4 = " mundo"
-print(message3 + message4)
