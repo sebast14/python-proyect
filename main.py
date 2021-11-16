601dd31fc1cf4e4178457df35304c4ceb59228d6
sebast14 <93997652+sebast14@users.noreply.github.com>
2021-11-16 12:21:59 -0500

    Restauracion tarea #1
    Lo borre por accidente intentando subir la tarea del dia de hoy (16/11/2021

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+message  =  "Espacio en memoria"
+número  =  12
+float_var  =  12.12
+nnumber1  =  12
+nnumber2  =  3
 
+nnumber1f  =  12,1
+nnumber2f  =  3,1
+
+imprimir ( mensaje )
+imprimir ( escribir ( mensaje ))
+
+imprimir ( número )
+imprimir ( tipo ( número ))
+
+imprimir ( float_var )
+imprimir ( tipo ( float_var ))
+
+message1  =  "Suma de dos int" 
+imprimir ( mensaje1 )
+
+print ( "Primer numero =" , nnumber1 )
+print ( "Segundo numero =" , nnumber2 )
+print ( "Resultado =" , nnumber1 + nnumber2 )
+
+message2  =  "Suma de dos float" 
+imprimir ( mensaje2 )
+
+print ( "Primer numero =" , nnumber1f )
+print ( "Segundo numero =" , nnumber2f )
+imprimir ( "Resultado =" , nnumber1f + nnumber2f )
+
+message3  =  "hola"
+message4  =  "mundo"
+imprimir ( mensaje3  +  mensaje4 )
