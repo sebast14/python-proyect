c8e14f14ad4f102ea9e3683b4ec9bb3a941f6944
Sebastian Mendoza Peña <juan.mendoza03@uptc.edu.co>
2021-12-02 18:38:50 -0500

    Calculadora_def

--- Calculadora1/main.py
+++ Calculadora1/main.py
@@ -1,44 +1,71 @@
 
+
+
+
 print("\nBienvenidos\n")
 print("Te pediremos dos numeros, para que puedas realizar la siguientes operaciones\n ")
 
 
 
-menuprincipal = int(input("Menu Principal \n" +
-                    "1. Suma \n" +
-                    "2. Resta \n" +
-                    "3. Multiplicacion \n" +
-                    "4. Divison \n" +
-                    "Elija una Opcion :) \n"))
+def menu_principal ():
+    print("Menu Principal \n" +
+    "1. Suma \n" +
+    "2. Resta \n" +
+    "3. Multiplicacion \n" +
+    "4. Divison \n" +
+    "Elija una Opcion :) \n")
+
+def ingrese_opcion():
+    return int(input())
 
 
-while menuprincipal != 0:
-    if menuprincipal == 1:
-        num1 = int(input("Digite el primer numero: "))
-        num2 = int(input("Digite el segundo numero: "))
-        result = num1 + num2
-        print("El resultado de la suma es: ", result)
-    elif menuprincipal == 2:
-        num3 = int(input("Digite el primer numero: "))
-        num4 = int(input("Digite el segundo numero: "))
-        result1 = num3 - num4
-        print("El resultado de la resta es: ", result1)
-    elif menuprincipal == 3:
-        num5 = int(input("Digite el primer numero: "))
-        num6 = int(input("Digite el segundo numero: "))
-        result2 = num5 * num6
-        print("El resultado de la multiplicacion es: ", result2)
-    elif menuprincipal == 4:
-        num7 = float(input("Digite el primer numero: "))
-        num8 = float(input("Digite el primer numero: "))
-        result3 = num7 / num8
-        if num8 == 0:
-            print("El divisor no puede ser igual a cero \n"+
-            "Utilice otro valor")
-        else:
-            print("el resultado de la division es: ", result3)
+menu_principal()
+option = ingrese_opcion()
+
+print("Digite su primer valor: ")
+n1 = float(input())
+print("Digite su segundo valor: ")
+n2 = float(input())
+
+def numeros(n1, n2):
+
+    return
+
+def suma_numero(n1, n2):
+    numeros(n1, n2)
+    result = n1 + n2
+    print("El resultado de la suma es: ",result)
+
+def resta_numero(n1, n2):
+    numeros(n1, n2)
+    result = n1 - n2
+    print("El resultado de la resta es: ", result)
+
+def multi_numero(n1, n2):
+    numeros(n1, n2)
+    result = n1 * n2
+    print("El resultado de la multiplicacion es: ", result)
+
+def divi_numero(n1, n2):
+    if n2 == 0:
+        print("No se puede dividir entre 0")
     else:
-        print("Espero haya sido de tu ayuda")
+        result = n1 / n2
+        print("El resultado de la division es " + str(result))
+
+
+
+if option == 1:
+    suma_numero(n1, n2)
+
+elif option == 2:
+    resta_numero(n1, n2)
+
+elif option == 3:
+    multi_numero(n1, n2)
+
+elif option == 4:
+    divi_numero(n1, n2)
 
 
 
