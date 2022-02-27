f140cbf4bec1447d1ca0e7cba62e3e5dae4e3316
sebast14 <93997652+sebast14@users.noreply.github.com>
2022-02-27 17:45:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,94 @@
-message  =  "Espacio en memoria"
-número  =  12
-float_var  =  12.12
-nnumber1  =  12
-nnumber2  =  3
 
-nnumber1f  =  12,1
-nnumber2f  =  3,1
 
-imprimir ( mensaje )
-imprimir ( escribir ( mensaje ))
 
-imprimir ( número )
-imprimir ( tipo ( número ))
 
-imprimir ( float_var )
-imprimir ( tipo ( float_var ))
 
-message1  =  "Suma de dos int" 
-imprimir ( mensaje1 )
 
-print ( "Primer numero =" , nnumber1 )
-print ( "Segundo numero =" , nnumber2 )
-print ( "Resultado =" , nnumber1 + nnumber2 )
+print("\nBienvenidos\n")
+print("Te pediremos dos numeros, para que puedas realizar la siguientes operaciones\n ")
 
-message2  =  "Suma de dos float" 
-imprimir ( mensaje2 )
 
-print ( "Primer numero =" , nnumber1f )
-print ( "Segundo numero =" , nnumber2f )
-imprimir ( "Resultado =" , nnumber1f + nnumber2f )
 
-message3  =  "hola"
-message4  =  "mundo"
-imprimir ( mensaje3  +  mensaje4 )
+def menu_principal ():
+    print("Menu Principal \n" +
+    "1. Suma \n" +
+    "2. Resta \n" +
+    "3. Multiplicacion \n" +
+    "4. Divison \n" +
+    "5. Salir \n\n" +
+    "Elija una Opcion :)\n")
+
+
+def ingrese_opcion():
+    return int(input())
+
+
+menu_principal()
+option = ingrese_opcion()
+
+if option == 5:
+    exit("Siento no haber podido ayudarte.")
+else:
+    print("Digite su primer valor:")
+    n1 = float(input())
+    print("Digite su segundo valor:")
+    n2 = float(input())
+
+
+
+while option <= 4:
+
+    def numeros(n1, n2):
+
+        return
+
+
+    def suma_numero(n1, n2):
+        numeros(n1, n2)
+        result = n1 + n2
+        print("El resultado de la suma es:  \n", result)
+
+
+    def resta_numero(n1, n2):
+        numeros(n1, n2)
+        result = n1 - n2
+        print("El resultado de la resta es:  \n", result)
+
+
+    def multi_numero(n1, n2):
+        numeros(n1, n2)
+        result = n1 * n2
+        print("El resultado de la multiplicacion es:  \n", result)
+
+
+    def divi_numero(n1, n2):
+        if n2 == 0:
+            print("No se puede dividir entre 0")
+        else:
+            result = n1 / n2
+            print("El resultado de la division es  \n" + str(result))
+
+
+    if option == 1:
+        suma_numero(n1, n2)
+
+    elif option == 2:
+        resta_numero(n1, n2)
+
+    elif option == 3:
+        multi_numero(n1, n2)
+
+    elif option == 4:
+        divi_numero(n1, n2)
+
+    menu_principal()
+    option = ingrese_opcion()
+
+    if option == 5:
+        exit("Espero que haya sido de mucha ayuda.\n"+
+             "Vuelve pronto.")
+    else:
+        print("Digite su primer valor:")
+        n1 = float(input())
+        print("Digite su segundo valor: ")
+        n2 = float(input())
