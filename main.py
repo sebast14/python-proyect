a3920432e39b9ecfd92bd674370a65e201b9f6fe
sebast14 <93997652+sebast14@users.noreply.github.com>
2022-03-28 00:03:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,115 @@
-message  =  "Espacio en memoria"
-número  =  12
-float_var  =  12.12
-nnumber1  =  12
-nnumber2  =  3
 
-nnumber1f  =  12,1
-nnumber2f  =  3,1
+import pandas as pd
 
-imprimir ( mensaje )
-imprimir ( escribir ( mensaje ))
 
-imprimir ( número )
-imprimir ( tipo ( número ))
+numeros_celular = []
 
-imprimir ( float_var )
-imprimir ( tipo ( float_var ))
+def add():
+    per=[]
+    per.append(input("Nombre : "))
+    per.append(input("Telefono: "))
+    per.append(input("Correo: "))
+    per.append(input("Cumpleaños: "))
+    return per
 
-message1  =  "Suma de dos int" 
-imprimir ( mensaje1 )
+def listar():
+    for per in numeros_celular:
+        print("Nombre: ", per[0])
+        print("Telefono: ", per[1])
+        print("Correo: ", per[2])
+        print("Cumpleaños: ", per[3])
+        print("º")
 
-print ( "Primer numero =" , nnumber1 )
-print ( "Segundo numero =" , nnumber2 )
-print ( "Resultado =" , nnumber1 + nnumber2 )
 
-message2  =  "Suma de dos float" 
-imprimir ( mensaje2 )
+dtmp = pd.read_csv("contactos.agenda")
+tmp = dtmp.values.tolist()
+for lin in tmp:
+     t = []
+     t.append(lin[1])
+     t.append(lin[2])
+     t.append(lin[3])
+     t.append(lin[4])
+     numeros_celular.append(t)
 
-print ( "Primer numero =" , nnumber1f )
-print ( "Segundo numero =" , nnumber2f )
-imprimir ( "Resultado =" , nnumber1f + nnumber2f )
+def modificar():
+    posicion=input("ingrese la posicion en la que se encuentra su contacto")
+    if posicion == "1":
+        numeros_celular[0] = add()
+        df = pd.DataFrame(numeros_celular, columns=["Nombre", "Telefono", "Correo", "Cumpleaños"])
+        df.to_csv("contactos.agenda")
+        agenda()
 
-message3  =  "hola"
-message4  =  "mundo"
-imprimir ( mensaje3  +  mensaje4 )
+#def eliminar_contacto():
+     #   eliminar_contacto = input("Nombre a eliminar: ")
+
+      #  add_archivo = open("contactos.agenda")
+       # t = []
+        #for linea in add_archivo:
+         #   valores = linea.strip().split(',')
+          #  t.append(valores)
+        #add_archivo.close()
+
+
+
+
+def agenda():
+    print("\n------AGENDA------")
+    print("1) Mis contactos.")
+    print("2) Buscar contacto.")
+    print("3) Agregar nuevo contacto.")
+    print("4) Editar contacto.")
+    print("5) Eliminar contacto.")
+    print("6) Salir.")
+
+    option = ""
+    while option not in ("1", "2", "3", "4", "5", "6"):
+        option = input("\nDijite la opcion deseada \n-> ")
+
+
+
+    if option == "1":
+        listar()
+        agenda()
+
+
+
+
+    if option == "3":
+        numeros_celular.append(add())
+        print("El numero de celular fue agragado con exito.")
+        agenda()
+
+    if option == "4":
+        modificar()
+        print("El contacto fue modificado con exito.")
+        agenda()
+
+    if option == "5":
+        eliminar_contacto()
+        print("Su contacto a sido eliminado con exito")
+        agenda()
+
+    if option == "6":
+
+        df = pd.DataFrame(numeros_celular, columns=["Nombre", "Telefono", "Correo", "Cumpleaños"])
+        df.to_csv("contactos.agenda")
+
+
+
+agenda()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
